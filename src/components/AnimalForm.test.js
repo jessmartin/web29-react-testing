dab72b47bd9553f30e60a49687d9fe8aebc84a04
Jess Martin <jessmartin@gmail.com>
2020-04-09 13:03:51 -0400

    Add some instructions to the AnimalForm.test.js

---
+++ src/components/AnimalForm.test.js
@@ -0,0 +1,7 @@
+// import the right stuff!
+
+test('Current Animals Test Species header is appearing', () => {
+  // arrange - render the component
+  // act - no action to take
+  // assert - get the text from the DOM and expect it to be in the document
+});
