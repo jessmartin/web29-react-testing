82e69f5506982c2177b7930841b067ef59d5dfa8
Jess Martin <jessmartin@gmail.com>
2020-04-09 13:06:04 -0400

    Make the test fail until it's implemented

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -4,4 +4,5 @@ test('Current Animals Test Species header is appearing', () => {
   // arrange - render the component
   // act - no action to take
   // assert - get the text from the DOM and expect it to be in the document
+  return false;
 });
